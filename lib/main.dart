7ca4e6080eafd02f0ee4712cd82c0acd69ffa16b
gorkemcirtma <210704312@st.maltepe.edu.tr>
2022-03-11 23:12:53 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+
+void main(){
+  runApp(
+    new Container(
+      child: new Center(
+        child: new Text("Hello world!", textDirection: TextDirection.ltr)
+      )
+    )
+
+
+  );
+
+
+}
