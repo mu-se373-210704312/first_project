9fe330bfcc5140f5a875242e3ab681f30ce0682c
gorkemcirtma <210704312@st.maltepe.edu.tr>
2022-03-15 23:04:40 +0300

    comit

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,45 @@ import "package:flutter/material.dart";
 
 
 void main(){
-  runApp(
-    new Container(
-      child: new Center(
-        child: new Text("Hello world!", textDirection: TextDirection.ltr)
-      )
-    )
+  runApp(myApp());
+}
 
+class myApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
 
-  );
+class MyAppState extends State<myApp>{
 
+  Widget build(BuildContext context){
+    return MaterialApp(
+        home:Scaffold(
+            appBar: AppBar(
+                title: Text("Shape"),
+            ),
+            body:Column(
+              children:<Widget> [Padding(padding: EdgeInsets.all(40.0)),
+                Container(alignment: Alignment.center,
+                  child :ElevatedButton(
+                    child: Text("Buton"),
+                    onPressed: (){},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      padding: const EdgeInsets.all(30),
+                      textStyle: TextStyle(fontSize: 20),
+                        alignment: Alignment.center
 
+                    ),),),
+                Text("Merhaba",
+                style: TextStyle(fontSize: 25),)
+                
+
+              ],
+            ),
+        )
+    );
+  }
 }
